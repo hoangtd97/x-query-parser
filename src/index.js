549dad9ba5fb9d9f4635c7d092506800a1770882
Trần Đình Hoàng <hoangtd97@outlook.com>
2019-04-17 16:26:10 +0700

    [INIT]

---
+++ src/index.js
@@ -0,0 +1,284 @@
+'use strict';
+
+const _get = require('lodash.get');
+
+const type_names = new Map([
+  [Number , 'number' ],
+  [String , 'string' ],
+  [Boolean, 'boolean'],
+  [Object , 'object' ],
+  [Array  , 'array'  ]
+]);
+
+const common_operators = [
+  {
+    key          : 'eq',
+    applyOnTypes : ['*'],
+    setter       : ({ it, field, value }) =>  set({ it, field, assign : { $eq : value } })
+  },
+  {
+    key          : 'gt',
+    applyOnTypes : [Number, Date],
+    setter       : ({ it, field, value }) =>  set({ it, field, assign : { $gt : value } })
+  },
+  {
+    key          : 'gte',
+    applyOnTypes : [Number, Date],
+    setter       : ({ it, field, value }) =>  set({ it, field, assign : { $gte : value } })
+  },
+  {
+    key          : 'lt',
+    applyOnTypes : [Number, Date],
+    setter       : ({ it, field, value }) =>  set({ it, field, assign : { $lt : value } })
+  },
+  {
+    key          : 'lte',
+    applyOnTypes : [Number, Date],
+    setter       : ({ it, field, value }) =>  set({ it, field, assign : { $lte : value } })
+  },
+  {
+    key          : 'in',
+    applyOnTypes : ['*'],
+    setter       : ({ it, field, value }) => {
+      let values = value.split(',');
+      set({ it, field, assign : { $in : values} });
+    }
+  },
+  {
+    key          : 'nin',
+    applyOnTypes : ['*'],
+    setter       : ({ it, field, value }) => {
+      let values = value.split(',');
+      set({ it, field, assign : { $nin : values} });
+    }
+  },
+  {
+    key          : 'like',
+    applyOnTypes : ['*'],
+    setter       : ({ it, field, value }) => {
+      set({ it, field, value : new RegExp(value, 'gi') });
+    }
+  },
+];
+
+function set({ it, field, value, assign }) {
+  if (field) {
+    if (it.alias[field]) {
+      field = it.alias[field];
+    }
+    if (!isAvailableField({ field, it })) {
+      return it.errors.push({
+        code    : 'ERR_UNAVAILABLE_FIELD',
+        field   : field,
+        message : `Can't search on field ${field}`
+      });
+    }
+    if (value !== undefined) {
+      it.filter[field] = value;
+    }
+    if (assign !== undefined) {
+      it.filter[field] = Object.assign({}, it.filter[field], assign);
+    }
+  }
+  else {
+    Object.assign(it.filter, assign);
+  }
+}
+
+function isAvailableField({ field, it }) {
+  if ((it.blackList.length > 0 && it.blackList.includes(field)) 
+    || (it.whiteList.length > 0 && !it.whiteList.includes(field))) {
+    return false;
+  }
+  return true;
+};
+
+function checkSchemaField({ schema, field }) {
+  let type = _get(schema, field, _get(schema, field.replace('.', '[0]')));
+
+  if (type.type !== undefined) {
+    type = type.type;
+  }
+
+  if (type !== undefined) {
+    return [true, type];
+  }
+
+  return [false];
+}
+
+const pagination_transpilers = [
+  {
+    matcher : ({ key }) => key === 'page',
+    setter  : ({ value, it }) => {
+      let page = parseInt(value);
+      if (Number.isNaN(page) || !(page > 0)) {
+        it.page = null;
+        it.errors.push({
+          code    : 'ERR_INVALID_TYPE',
+          message : 'page must be an integer and greater than 0',
+          key     : 'page'
+        });
+        return;
+      }
+      it.page = page;
+    },
+  },
+  {
+    matcher : ({ key }) => key ==='limit',
+    setter : ({ value, it }) => {
+      let limit = parseInt(value);
+      if (Number.isNaN(limit) || !(limit >= 0)) {
+        it.limit = null;
+        it.errors.push({
+          code    : 'ERR_INVALID_TYPE',
+          message : 'limit must be an positive integer',
+          key     : 'limit'
+        });
+        return;
+      }
+      it.limit = limit;
+    },
+  },
+  {
+    matcher : ({ key }) => key ==='sort',
+    setter : ({ value, it }) => {
+      let sort     = {};
+      let raw_sort = value;
+
+      let list_field_direction = raw_sort.split(',');
+
+      for (let field_direction of list_field_direction) {
+        if (field_direction.endsWith('_asc')) {
+          let field = field_direction.slice(0, -('_asc'.length));
+          sort[field] = 1;
+          continue;
+        }
+        if (field_direction.endsWith('_desc')) {
+          let field = field_direction.slice(0, -('_desc'.length));
+          sort[field] = -1;
+          continue;
+        }
+        it.errors.push({
+          code : 'INVALID_SORT',
+          value : field_direction
+        });
+        return;
+      }
+
+      it.sort = sort;
+    },
+  }
+];
+
+const key_operator_transpliers = [
+  {
+    matcher  : ({ key }) => common_operators.find(operator => key.endsWith('_' + operator.key)),
+    setter   : ({ key, value, it }) => {
+      let operator = common_operators.find(operator => key.endsWith('_' + operator.key));
+      let field = key.slice(0, -(operator.key.length + 1));
+
+      let [schema_has_field, type] = checkSchemaField({ schema : it.schema, field });
+
+      if (!schema_has_field) {
+        return it.errors.push({
+          code : 'ERR_INVALID_FIELD',
+          field : field,
+          message : `Invalid field ${field}`
+        });
+      }
+
+      if (operator.applyOnTypes[0] !== '*' && !operator.applyOnTypes.includes(type)) {
+        let type_name = type_names.get(type);
+        return it.errors.push({
+          code     : 'ERR_WRONG_OPERATOR',
+          field    : field, 
+          type     : type_name,
+          operator : operator.key,
+          message  : `Can't use operator ${operator.key} on ${field} has type ${type_name}`
+        });
+      }
+
+      operator.setter({ it, field, value });
+    },
+  },
+  {
+    name    : 'equal',
+    matcher : () => true,
+    setter  : ({ key, value, it }) => {
+      let field = key;
+      if (isAvailableField({ field, it })) {
+        set({ it, field, value });
+      }
+    },
+  }
+];
+
+const DEFAULT_TRANSPILERS = [].concat(pagination_transpilers).concat(key_operator_transpliers);
+
+function transpile({ transpilers = DEFAULT_TRANSPILERS, query, it }) {
+  for (let key in query) {
+    let value = query[key];
+
+    for (let { matcher, setter } of transpilers) {
+      if (matcher({ key, value, it })) {
+        setter({ key, value, it });
+        break;
+      }
+    }
+  }
+}
+
+function Parser({ schema, required = [], blackList = [], whiteList = [], defaults, custom = {}, alias = [] }) {
+  if (typeof schema.obj === 'object') {
+    schema = schema.obj;
+  }
+
+  let custom_transpilers = [];
+
+  for (let custom_key in custom) {
+    custom_transpilers.push({
+      matcher : ({ key }) => key === custom_key,
+      setter  : ({ it, key, value }) => {
+        let custom_setter = custom[custom_key];
+        let custom_value = custom_setter(value);
+
+        set({ it, assign : custom_value });
+      }
+    });
+  }
+
+  let transpilers = custom_transpilers.concat(DEFAULT_TRANSPILERS);
+
+  return function parse(query) {
+    let errors = [];
+
+    const it = { errors, schema, required, blackList, whiteList, defaults, custom, alias };
+  
+    Object.assign(it, { filter : {} }, defaults);
+  
+    transpile({ transpilers, it, query });
+
+    // final process
+
+    if (Array.isArray(it.required) && it.required.length > 0) {
+      for (let field of it.required) {
+        if (it.filter[field] === undefined) {
+          it.errors.push({
+            code : 'ERR_REQUIRED',
+            field : field,
+            message : `${field} is required`
+          });
+        }
+      }
+    }
+
+    if (it.page > 0 && it.limit > 0) {
+      it.skip = (it.page - 1) * it.limit;
+    }
+
+    return it;
+  }
+}
+
+module.exports = { Parser, isAvailableField, checkSchemaField, set };
---
+++ src/index.spec.js
@@ -0,0 +1,118 @@
+'use strict';
+
+const assert   = require('assert');
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+const { Parser }  = require('..');
+
+const OrderSchema = new Schema({
+  shop_id  : { type : Number },
+  id       : { type : Number },
+  customer : {
+    id   : { type : Number },
+    name : { type : String },
+    phone : { type : String },
+  },
+  line_items : [{
+    id       : { type : Number },
+    barcode  : { type : String },
+    quantity : { type : Number }
+  }],
+  created_at    : { type : Date },
+  status        : { type : String },
+  private_field : { type : Number },
+  order_number  : { type : String },
+});
+
+const parse = Parser({
+  schema    : OrderSchema,
+  required  : ['shop_id'],
+  blackList : ['private_field'],
+  whiteList : [],
+  alias     : {
+    barcode : 'line_items.barcode'
+  },
+  defaults  : {
+    page  : 1,
+    limit : 20,
+    sort  : 'created_at_asc',
+  },
+  custom : {
+    keyword : (value) => { return { $or : [
+      { 'order_number'   : new RegExp(value, 'gi') },
+      { 'customer.phone' : new RegExp(value, 'gi') }
+    ]}} 
+  }
+});
+
+it ('should parser query to mongoose filter successfully', () => {
+
+  let query = {
+    'shop_id'              : 100000001,
+    'created_at_gte'       : '2019-04-01',
+    'created_at_lte'       : '2019-04-30',
+    'customer.name_like' : 'hoang',
+    'barcode'              : 'HEO',
+    'status_in'            : 'NEW,ASSIGN_EMPLOYEE',
+    'keyword'              : '0968726159',
+    // pagination
+    'page'                 : 2, 
+    'limit'                : 20,
+    'sort'                 : 'created_at_asc,id_desc'
+  };
+
+  let { errors, page, filter, skip, limit, sort } = parse(query);
+
+  let expectedFilter = {
+    'shop_id' : 100000001,
+    'created_at' : { 
+      $gte : '2019-04-01',
+      $lte : '2019-04-30'
+    },
+    'customer.name'      : new RegExp('hoang', 'gi'),
+    'line_items.barcode' : 'HEO',
+    'status'             : { $in : ['NEW', 'ASSIGN_EMPLOYEE'] },
+    '$or' : [
+      { 'order_number'   : new RegExp('0968726159', 'gi') },
+      { 'customer.phone' : new RegExp('0968726159', 'gi') }
+    ],
+  };
+
+  assert.deepEqual(filter, expectedFilter);
+  assert.equal(page, 2);
+  assert.equal(skip, 20);
+  assert.equal(limit, 20);
+  assert.deepEqual(sort, { created_at : 1, id : -1 });
+});
+
+it ('should parser query to mongoose filter fail when mis required field and use wrong operator', () => {
+
+  let query = {
+    'customer.name_gte' : 'hoang',
+    private_field_gt : 10,
+  };
+
+  let { errors, filter } = parse(query);
+
+  let expectedErrors = [
+    {
+      code : 'ERR_WRONG_OPERATOR',
+      field : 'customer.name',
+      type : 'string',
+      operator : 'gte',
+      message : `Can't use operator gte on customer.name has type string`
+    },
+    {
+      code    : 'ERR_UNAVAILABLE_FIELD',
+      field   : 'private_field',
+      message : `Can't search on field private_field`
+    },
+    {
+      code : 'ERR_REQUIRED',
+      field : 'shop_id',
+      message : 'shop_id is required'
+    }
+  ];
+
+  assert.deepEqual(errors, expectedErrors);
+});
